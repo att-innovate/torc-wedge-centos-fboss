fc3a621575ff21f394d7e079e6c873908c0566ff
Marcel Neuhausler <neuhausler@gmail.com>
2016-05-25 14:29:25 -0700

    script to run container .. work in progress

--- wedge/scripts/run-fboss-container.sh
+++ wedge/scripts/run-fboss-container.sh
@@ -1,2 +1,7 @@
 #/bin/bash
 
+echo 950000 > /cgroup/cpu/docker/cpu.rt_runtime_us
+
+ID=$(docker run -d --privileged=true --net=host --name=fboss-2.0 --volume=/tmp/:/logs/ fboss-2.0)
+echo 950000 > /cgroup/cpu/docker/$ID/cpu.rt_runtime_us
+
