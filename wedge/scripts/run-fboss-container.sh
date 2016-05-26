010884cf055f1bdc5da5af42c07a1cc9f12cdaae
Marcel Neuhausler <neuhausler@gmail.com>
2016-05-26 15:52:32 -0700

    fix run-fboss container

--- wedge/scripts/run-fboss-container.sh
+++ wedge/scripts/run-fboss-container.sh
@@ -1,7 +1,23 @@
 #/bin/bash
 
+# remove previous log-files
+rm -rf /tmp/wedge_agent*
+
+# increase thread run time in general
 echo 950000 > /cgroup/cpu/docker/cpu.rt_runtime_us
 
+# start container with backdoor
 ID=$(docker run -d --privileged=true --net=host --name=fboss-2.0 --volume=/tmp/:/logs/ fboss-2.0)
+
+# increase thread run time for this contaienr
 echo 950000 > /cgroup/cpu/docker/$ID/cpu.rt_runtime_us
+
+# wait for everything to be ready
+sleep 5
+
+# start fboss through backdoor
+curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" 127.0.0.1:8085/async -d '{"cmd":"/start-fboss.sh"}'
+
+# verify log files
+echo check log files from wedge_agent in /tmp
 
